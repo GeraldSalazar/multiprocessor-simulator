22c5d141d54725cd7305585328924b4ff9dd64be
GeraldSalazar <geraldsalazar4@gmail.com>
2022-09-15 19:47:20 -0600

    random operation generator function

--- src/app/cpus-web-workers/cpu1.worker.ts
+++ src/app/cpus-web-workers/cpu1.worker.ts
@@ -1,15 +1,18 @@
 /// <reference lib="webworker" />
 import { generateRandomDir } from './shared/instruc-generation/random-dir-generator';
-import { generateRandomInstruction } from './shared/instruc-generation/random-instruction-gen'
+import { generateRandomOperation } from './shared/instruc-generation/random-operation-gen'
 
 addEventListener('message', ({ data }) => {
   const response = `worker response to ${data}`;
   console.log(response)
   const randomDirs = []
+  const randomOperations = []
   for(let i=0; i<25; i++) {
     //console.log(generateRandomInstruction())
     randomDirs.push(generateRandomDir())
+    randomOperations.push(generateRandomOperation())
   }
   console.log(randomDirs)
+  console.log(randomOperations)
   //postMessage(response);
 });
